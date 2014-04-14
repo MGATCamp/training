029ac5bc3b93bdfd7d5aa84defb291bad17819b0
lewenyong <lewenyong@163.com>
2014-04-14 23:09:30 +0800

    Update middle.java

--- basic/middle.java
+++ basic/middle.java
@@ -1,4 +1,8 @@
-public static int middleNum(int[] a) {
+package com.lewenyong;
+
+public class Main {
+
+	public static int middleNum(int[] a) {
 		int len = a.length;
 		int m = (len - 1) / 2;
 		for (int i=0;i<len; i++) {
@@ -17,3 +21,10 @@ public static int middleNum(int[] a) {
 		}
 		throw new RuntimeException("error");
 	}
+
+	public static void main(String[] args) {
+		int[] a = new int[] { 100, 4, 4, 4, 4, 101, 7, 5, 4 };
+		System.out.println(middleNum(a));
+ 	}
+
+}
