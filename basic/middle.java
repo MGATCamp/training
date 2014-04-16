7ec051812a07be955c6c98ae7c640b62b290622d
lewenyong <lewenyong@163.com>
2014-04-16 20:24:48 +0800

    Update middle.java

--- basic/middle.java
+++ basic/middle.java
@@ -1,30 +1,64 @@
 package com.lewenyong;
 
-public class Main {
+public class Test {
 
-	public static int middleNum(int[] a) {
-		int len = a.length;
-		int m = (len - 1) / 2;
-		for (int i=0;i<len; i++) {
-			int count = 0;
-			for (int j=0 ;j<len;j++) {
-				if (a[i] >= a[j]&&i!=j) {
-					count++;
-					if(count>m){
-						break;
-					}
-				}
-			}
-			if(count==m){
-				return a[i];
-			}
-		}
-		throw new RuntimeException("error");
-	}
+    public static int middle(int a[]) {
+        return getIndex(a, (a.length - 1) / 2);
+    }
 
-	public static void main(String[] args) {
-		int[] a = new int[] { 100, 4, 4, 4, 4, 101, 7, 5, 4 };
-		System.out.println(middleNum(a));
- 	}
+    public static int getIndex(int a[], int index) {
+        if (index > a.length - 1) {
+            throw new IllegalArgumentException();
+        }
+        if (a.length == 1) {
+            return a[0];
+        }
+        //随机取一个数小于或等于index
+        Random random = new Random();
+        int r = random.nextInt(a.length - 1);
+
+        int rv = a[r];
+
+        int[] aLeft = new int[a.length];
+        int[] aRight = new int[a.length];
+
+        int lIndex = 0;
+        int rIndex = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            if (i != r) {
+                if (a[i] < rv) {
+                    aLeft[lIndex++] = a[i];
+                } else {
+                    aRight[rIndex++] = a[i];
+                }
+            }
+        }
+
+        aLeft = Arrays.copyOfRange(aLeft, 0, lIndex);
+        aRight = Arrays.copyOfRange(aRight, 0, rIndex);
+
+        //计算当前重复的
+        int repeatNum = a.length - aLeft.length - aRight.length;
+
+        if (index <= aLeft.length - 1) {
+            //寻找的值在左侧
+            a = aLeft;
+            //index不变
+        } else if (index < aLeft.length + repeatNum) {
+            return rv;
+
+        } else {
+            a = aRight;
+            index = index - aLeft.length - 1;
+        }
+
+        return getIndex(a, index);
+    }
+
+    public static void main(String[] args) {
+        int a[] = new int[] { 2, 5, 5, 5, 6, 6, 9 };
+        System.out.println(middle(a));
+    }
 
 }
